9da3209b0a98984e3ff80416e0cf9040d87f6e10
Panduka Muditha <pandukamudhitha@gmail.com>
2018-06-12 19:06:48 +0530

    Revert "Admin module"

--- src/app/core/models/project.ts
+++
@@ -1,8 +0,0 @@
-import { AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Task } from './task';
-
-export interface Project {
-    id?: string;
-    name: string;
-    tasks: AngularFirestoreCollection<Task>;
-}
