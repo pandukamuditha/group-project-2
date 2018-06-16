a1fde756f5c25560e3be347ba511656424c22cdb
JayanakaFonseka <jayanakafonseka@gmail.com>
2018-06-16 10:06:02 +0530

    routing errors

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeComponent } from './employee.component';
+import { LandingModule } from './landing/landing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
-  declarations: []
+  declarations: [
+    EmployeeComponent,
+    LandingComponent
+  ],
+  exports: [  ]
 })
 export class EmployeeModule { }
--- src/app/employee/landing/landing.module.ts
+++ src/app/employee/landing/landing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
+import { CardComponent } from './card/card.component';
+import { LandingComponent } from './landing.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    AppRoutingModule
   ],
-  declarations: []
+  declarations: [
+    LandingComponent,
+    CardComponent
+  ],
+  exports: [
+    LandingComponent,
+    CardComponent
+  ]
 })
 export class LandingModule { }
