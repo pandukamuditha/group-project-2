021f45926385e0f3b76ae1f700df5bc9109e9223
Panduka Muditha <pandukamudhitha@gmail.com>
2018-06-20 23:13:28 +0530

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeModule } from './employee/employee.module';
 import { HumanResourceModule } from './human-resource/human-resource.module';
 import { FormsModule } from '@angular/forms';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -15,14 +15,12 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    AdminModule,
     EmployeeModule,
-    HumanResourceModule,
-    FormsModule
-    BrowserModule,
-    AppRoutingModule,
-    AdminModule
+    HumanResourceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
