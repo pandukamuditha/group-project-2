2746881c76b36098281581b55048528514a22c3a
JayanakaFonseka <jayanakafonseka@gmail.com>
2018-08-22 09:47:57 +0530

    hrmanager nodejs removed

--- NodeJS/models/employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var Employee = mongoose.model('Employee', {
-    name: { type: String },
-    position: { type: String },
-    office: { type: String }
-});
-
-module.exports = { Employee }; 
